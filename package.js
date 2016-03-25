05bc19dbb650b6e72757fea7d5844e5202ae15d7
Eric Dobbertin <eric@dairystatedesigns.com>
2016-03-24 21:11:11 -0500

    Add force-non-portable and publish

--- package.js
+++ package.js
@@ -2,11 +2,12 @@ Package.describe({
   name: "dispatch:phantomjs-tests",
   summary: "A helper package for Meteor test driver packages. Runs client tests in PhantomJS.",
   git: "https://github.com/dispatch/meteor-phantomjs-tests.git",
-  version: '0.0.3',
+  version: '0.0.4',
   testOnly: true,
 });
 
 Npm.depends({
+  'meteor-force-non-portable': '1.0.0',
   'phantomjs-prebuilt': '2.1.5',
 });
 

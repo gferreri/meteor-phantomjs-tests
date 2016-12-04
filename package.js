138adb42c79aed5de52865b8cdad0075a3d21d69
Eric Dobbertin <eric@dairystatedesigns.com>
2016-12-04 15:41:13 -0600

    Update to latest phantomjs-prebuilt and publish 0.0.6

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
   name: "dispatch:phantomjs-tests",
   summary: "A helper package for Meteor test driver packages. Runs client tests in PhantomJS.",
   git: "https://github.com/dispatch/meteor-phantomjs-tests.git",
-  version: '0.0.5',
+  version: '0.0.6',
   testOnly: true,
 });
 
 Npm.depends({
   'meteor-force-non-portable': '1.0.0',
-  'phantomjs-prebuilt': '2.1.7',
+  'phantomjs-prebuilt': '2.1.13',
 });
 
 Package.onUse(function (api) {
